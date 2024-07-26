7f87430ac579f765ff8e04ef86666fd01a1f59b9
Yusuf Can Mercan <cusufcan@gmail.com>
2024-07-26 20:32:47 +0300

    storage dependency removed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
-    implementation(libs.firebase.storage)
 
     // Navigation
     implementation(libs.androidx.navigation.fragment.ktx)
